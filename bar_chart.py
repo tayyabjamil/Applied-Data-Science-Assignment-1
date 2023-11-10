a7d60447668e5dafe0baa9dcca2796c4c3f67477
tayyab jamil <50493469+tayyabjamil@users.noreply.github.com>
2023-11-10 17:15:56 +0000

    removed extra function to simplify code

--- bar_chart.py
+++ bar_chart.py
@@ -2,54 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def clean_data(world_cup_data):
-    """
-    This function takes data as parameter, loops through all row items,
-    and removes rows that contain non-numeric or 'NAN' values.
-    The cleaned data is converted into a DataFrame for plotting.
-    """
-    cleaned_data = []
-
-    for index, row in world_cup_data.iterrows():
-        is_valid_row = True
-
-        # Check each column in the row
-        for column in world_cup_data.columns:
-            cell_value = row[column]
-
-            # Check if the cell is None or contains 'NAN' as a string
-            if cell_value is None or cell_value == "NAN":
-                is_valid_row = False
-                break
-
-        if is_valid_row:
-            cleaned_data.append(row)
-
-    cleaned_data_frame = pd.DataFrame(
-        cleaned_data, columns=world_cup_data.columns)
-    return cleaned_data_frame
-
-
 def read_data():
     """
     Read World cup 2023 matches data from csv file
-    Pass data to clean function to exclude any entry
-    with value NAN
-    Extract Columns required for bar graph
+    Extract Coloumns required for bar grpah
     """
-    # Load your dataset
+# Load your dataset
     world_cup_data = pd.read_csv("wc_matches.csv")
-    # Calling clean_data function
-    cleaned_data = clean_data(world_cup_data)
 
-    # Extracting two teams and their score columns
+# Extracting two teams and their score columns
     columns_to_extract = ['team1', 'team2', 'score1', 'score2']
-    all_matches = cleaned_data[columns_to_extract]
+    all_matches = world_cup_data[columns_to_extract]
     dataframe_allmatches = pd.DataFrame(all_matches)
     return dataframe_allmatches
-
-
 # Create a function to determine the winner
+
+
 def get_winner(row):
     if row['score1'] > row['score2']:
         return row['team1'], row['score1']
@@ -58,14 +26,16 @@ def get_winner(row):
     else:
         return row['score2']
 
-
 # Apply the function to each row to get the winners
-def get_scores_of_winning_team(world_cup_data):
+
+
+def getScoresofWinningTeam(world_cup_data):
     """
-    Get data of every match played by teams
-    Use get_winner function to decide which team won the match
+    Get data of every match played by teams 
+    Use getWinner function to decide which team won the match
     add winner team and winning score columns
-    use group by to calculate all winning score by each team
+    use group by of calculate all winning score by each team
+
     """
     world_cup_data[['winner', 'winning_score']] = world_cup_data.apply(
         get_winner, axis=1, result_type='expand')
@@ -81,6 +51,7 @@ def get_scores_of_winning_team(world_cup_data):
 
 
 def show_bar_graph(teams_and_scores):
+
     plt.figure(figsize=(12, 6))
 
     plt.bar(teams_and_scores['team1'], teams_and_scores['winning_score'])
@@ -98,6 +69,6 @@ def show_bar_graph(teams_and_scores):
 # Calling Function 1
 world_cup_data = read_data()
 # Calling Function 2
-teams_and_scores = get_scores_of_winning_team(world_cup_data)
+teams_and_scores = getScoresofWinningTeam(world_cup_data)
 # Calling Function 3
 show_bar_graph(teams_and_scores)
